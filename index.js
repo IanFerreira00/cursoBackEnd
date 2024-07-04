5a5fa266323d34a3bd5a825a718731d772e55ecc
IAN MURAD ARAÚJO FERREIRA <ian_ferreira@estudante.sesisenai.org.br>
2024-07-04 16:28:57 -0300

    atualizado

--- index.js
+++ index.js
@@ -1,13 +1,9 @@
-     function filtroPar(arr){
-        nova_arr = []
-        for(let i=0; i<arr.length ; i++){
-            if(arr[i] % 2 === 0){
-                nova_arr.push(arr[i])
-            }
-        }
-        
-        return nova_arr
-    }
-    arr = [1,2,3,4,5]
-    resultado = filtroPar(arr)
-    console.log(resultado)
+const express = require('express')
+
+const app = express()
+
+app.get('/test-api', function (req, res){
+    res.send('NOSSA API TA FUNCIONANDO')
+})
+
+app.listen(8000)
