a7ebac3c302828c6bd2e18eab35f67b78f953e05
IAN MURAD ARAÚJO FERREIRA <ian_ferreira@estudante.sesisenai.org.br>
2024-07-04 17:27:59 -0300

    att

--- index.js
+++ index.js
@@ -1,6 +1,26 @@
 const express = require('express')
-
+const {Client} = require('pg')
+require("dotenv").config()
 const app = express()
+const client = new Client({
+    host:process.env.host,
+    port:process.env.port,
+    user:process.env.user,
+    password:process.env.password,
+    database:process.env.database
+})
+
+const connectDB = async () => {
+    client
+    .connect()
+    .then(() => {
+        console.log('a conexao funcionou');
+    }).catch((err) => {
+    console.error('erro ao conectar na bd')
+    });
+};
+connectDB()
+
 
 app.get('/test-api', function (req, res){
     res.send('NOSSA API TA FUNCIONANDO')
